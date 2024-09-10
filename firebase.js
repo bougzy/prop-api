606413007e4b4f710e74e6c8d5c3771bfb1c9239
bougzy <yubeespecial@gmail.com>
2024-09-10 19:12:51 +0100

    balll

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDINwVhv_MqLf4dScWzc72_Vx2eD3InSec",
+    authDomain: "connectbayelsa-d89f7.firebaseapp.com",
+    projectId: "connectbayelsa-d89f7",
+    storageBucket: "connectbayelsa-d89f7.appspot.com",
+    messagingSenderId: "1081084806786",
+    appId: "1:1081084806786:web:1a90d33dba8cb06b59d69b",
+    measurementId: "G-04MEF8KQ0K"
+  };
+  
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
